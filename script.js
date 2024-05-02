c4196dfdd3d4c16376a937eb7ef5b1f31ae8ec6e
Stoyots <tom.benchtrit@gmail.com>
2024-05-03 00:09:10 +0200

    changements mineurs et ajout section presentation

--- script.js
+++ script.js
@@ -178,8 +178,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const offsetY = (clientY - top) / height;
     const offsetX = (clientX - left) / width;
 
-    const rotateX = (offsetY - 0.2) * -5;
-    const rotateY = (offsetX - 0.2) * -5;
+    const rotateX = (offsetY - 0.2) * -2;
+    const rotateY = (offsetX - 0.2) * -2;
 
     // Appliquer la transformation à la section
     section.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
